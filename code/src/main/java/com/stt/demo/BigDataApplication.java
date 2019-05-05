4779a8ef004df1c1c387edf3b9b686fd41d4dce0
shetengteng <work_stt@163.com>
2019-05-05 23:44:09 +0800

    mapReduce

--- code/src/main/java/com/stt/demo/BigDataApplication.java
+++ code/src/main/java/com/stt/demo/BigDataApplication.java
@@ -3,11 +3,11 @@ package com.stt.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class BigDataApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BigDataApplication.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(BigDataApplication.class, args);
+//	}
 
 }
