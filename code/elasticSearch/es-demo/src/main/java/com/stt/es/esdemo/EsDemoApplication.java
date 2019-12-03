76da34cc50d075735ff93ecb7b186b4c8250ed30
shetengteng <work_stt@163.com>
2019-12-03 19:23:55 +0800

    machine learnning

--- code/elasticSearch/es-demo/src/main/java/com/stt/es/esdemo/EsDemoApplication.java
+++ code/elasticSearch/es-demo/src/main/java/com/stt/es/esdemo/EsDemoApplication.java
@@ -3,11 +3,43 @@ package com.stt.es.esdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.ref.WeakReference;
+
 @SpringBootApplication
 public class EsDemoApplication {
 
+//	public static void main(String[] args) {
+//		SpringApplication.run(EsDemoApplication.class, args);
+//	}
+
+
 	public static void main(String[] args) {
-		SpringApplication.run(EsDemoApplication.class, args);
+		Car car = new Car(2000.0, "red");
+		WeakReference<Car> wrc = new WeakReference<>(car);
+		int i = 0;
+		while (true)
+		{
+			if (wrc.get() != null)
+			{
+				i++;
+				System.out.println("WeakReferenceCar's Car is alive for " + i + ", loop - " + wrc);
+			}
+			else
+			{
+				System.out.println("WeakReferenceCar's Car has bean collected");
+				break;
+			}
+		}
 	}
 
+	static class Car {
+		private double     price;
+		private String    color;
+
+		public Car(double price, String color)
+		{
+			this.price = price;
+			this.color = color;
+		}
+	}
 }
---
+++ code/scala-demo/src/main/scala/com/Test.scala
@@ -0,0 +1,19 @@
+
+object Test{
+  def main(args: Array[String]): Unit = {
+    val plus3 = (x: Int) => {
+      println("plus3")
+      x+3
+    }
+    println(plus3(1))
+
+    val f1 = (n1:Int,n2:Int) => n1+n2
+    println("f1的类型="+f1)
+    println(f1(1,2))
+  }
+}
+
+//plus3
+//4
+//f1的类型=<function2>
+//3
